990e456d91a9f0e595d68bf404a44003afac59ef
Antoine Favarel <antoine.favarel@epitech.eu>
2014-11-03 13:38:38 +0100

    ttes

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 ## Login   <winerzk@epitech.net>
 ## 
 ## Started on  Sat Nov  1 12:20:48 2014 Antoine Favarel
-## Last update Sat Nov  1 12:49:48 2014 Antoine Favarel
+## Last update Mon Nov  3 13:38:25 2014 Antoine Favarel
 ##
 
 echo "Instalation de turbin.."
@@ -13,11 +13,8 @@ rm -fr ~/tmp/turbin
 mkdir -p ~/tmp/
 git clone https://github.com/EnsenHe/turbin.git ~/tmp/turbin/
 sudo rm -fr "/usr/bin/turbin"
-sudo rm -fr "/usr/bin/turbin_serv.rb"
 chmod 777 ~/tmp/turbin/turbin.rb
-chmod 777 ~/tmp/turbin/turbin_serv.rb
 user=$USER
 sudo ln -s "/home/$user/tmp/turbin/turbin.rb" "/usr/bin/turbin"
-sudo ln -s "/home/$user/tmp/turbin/turbin_serv.rb" "/usr/bin/turbin_serv.rb"
 echo "Instalation de turbin terminé"
 echo "usage : turbin [start | stop]"
